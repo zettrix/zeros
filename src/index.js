7b04ab48f5c7495274db56aad4291e4ca7f62f0b
zettrix <fibreun@gmail.com>
2018-09-26 22:59:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var res = 0,
+      pow = 0
+  for(var i = 1; pow < number; i++){
+      var pow = Math.pow(5,i);
+      res += Math.floor(number/pow);
+  }
+  return res;
 }
